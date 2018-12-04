4b6c472c1d75736ca851e56ab53dbbc2e0e1bbd9
bigDreamer <1826001146@qq.com>
2018-12-04 20:37:56 +0800

    v-echarts

---
+++ echarts/precache-manifest.360fa340055bac6a2e307b2d5ea97e1b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "481e435d747b9779d6ed",
+    "url": "public/js/index.b8d22df0.js"
+  },
+  {
+    "revision": "c7378c5bd14f4b55b26e",
+    "url": "public/js/chunk-vendors.f7de9662.js"
+  },
+  {
+    "revision": "e6a2e299d4cf38ed23fff6abd25b825c",
+    "url": "index.html"
+  }
+];
